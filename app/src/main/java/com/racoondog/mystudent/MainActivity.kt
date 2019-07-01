5d6ad647d244be92063fc088283e8a34f851b8f6
Insoo Kim <heyinsoo@gmail.com>
2019-07-01 19:28:48 +0900

    fabric 연동

--- app/src/main/java/com/racoondog/mystudent/MainActivity.kt
+++ app/src/main/java/com/racoondog/mystudent/MainActivity.kt
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.crashlytics.android.Crashlytics
 import io.fabric.sdk.android.Fabric
+import com.crashlytics.android.answers.ContentViewEvent
+import com.crashlytics.android.answers.Answers
+
+
 
 
 
@@ -12,6 +16,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Fabric.with(this, Crashlytics())
+        Fabric.with(this, Answers())
         setContentView(R.layout.activity_main)
+
+        // TODO: Use your own attributes to track content views in your app
+        Answers.getInstance().logContentView(
+            ContentViewEvent()
+                .putContentName("Tweet")
+                .putContentType("Video")
+                .putContentId("1234")
+                .putCustomAttribute("Favorites Count", 20)
+                .putCustomAttribute("Screen Orientation", "Landscape")
+        )
+
     }
 }
