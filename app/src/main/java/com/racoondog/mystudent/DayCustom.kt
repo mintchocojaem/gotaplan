2cdab4b57c0514fd36699c3c1d0716b3a167e58f
Hades7014 <ljm70148650@gmail.com>
2019-10-10 12:27:20 +0900

    뷰 제작 v1

--- app/src/main/java/com/racoondog/mystudent/DayCustom.kt
+++ app/src/main/java/com/racoondog/mystudent/DayCustom.kt
@@ -4,16 +4,22 @@ import android.content.Context
 import android.content.res.TypedArray
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
 import kotlinx.android.synthetic.main.day_layout.view.*
 
-class DayCustom (context: Context?) : ConstraintLayout(context){
-    constructor(context: Context, attrs: AttributeSet) : this(context) {
+class DayCustom  : ConstraintLayout {
+
+    constructor(context: Context?) : super(context, null) {
+        initView()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context,attrs) {
         initView()
         getAttrs(attrs)
     }
 
-    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : this(context, attrs) {
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle) {
         initView()
         getAttrs(attrs, defStyle)
 
@@ -40,7 +46,7 @@ class DayCustom (context: Context?) : ConstraintLayout(context){
         var backgroundResourceId = typedArray.getResourceId(R.styleable.LoginButton_bg, R.color.naver_color)
         bgLinear.setBackgroundResource(backgroundResourceId)
         */
-        val symbolResourceId = typedArray.getResourceId(R.styleable.day_button_bg, R.drawable.mintmdpi)
+        val symbolResourceId = typedArray.getResourceId(R.styleable.day_button_bg, R.drawable.mintxxhdpi)
         day.setImageResource(symbolResourceId)
         /*
         var textColor = typedArray.getColor(R.styleable.LoginButton_textColor, 0)
