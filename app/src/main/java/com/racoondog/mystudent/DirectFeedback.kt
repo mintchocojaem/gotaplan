0e4a90dc3ab9f733967085197dcd3a229c4d86f9
void <void0629@gmail.com>
2020-01-31 06:14:49 +0900

    다이렉트 피드백 기능 구현

--- app/src/main/java/com/racoondog/mystudent/DirectFeedback.kt
+++ app/src/main/java/com/racoondog/mystudent/DirectFeedback.kt
@@ -1,5 +1,6 @@
 package com.racoondog.mystudent
 
+import android.app.Activity
 import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.os.Bundle
@@ -8,6 +9,9 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.direct_feedback_layout.*
 
 class DirectFeedback:AppCompatActivity() {
+
+    private var mailClientOpened = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.direct_feedback_layout)
@@ -18,20 +22,44 @@ class DirectFeedback:AppCompatActivity() {
 
         directFeedbackSend_Button.setOnClickListener {
 
-           val email = Intent(Intent.ACTION_SEND)
-
             val title = directFeedback_title_text.text.toString()
             val content = directFeedback_content_text.text.toString()
             val address = arrayOf<String>("void0629@gmail.com")
 
-            email.setType("message/rfc822")
+            val email = Intent(Intent.ACTION_SEND)
+            email.setType ("text/plain")
+            val resInfo = packageManager.queryIntentActivities(email, 0)
+            if (!resInfo.isEmpty())
+            {
+                for (info in resInfo)
+                {
+                    if (info.activityInfo.packageName.toLowerCase().contains("android.gm") || info.activityInfo.name.toLowerCase().contains("android.gm"))
+                    {
+                        email.putExtra(Intent.EXTRA_EMAIL, address)
+                        email.putExtra(Intent.EXTRA_SUBJECT, title)
+                        email.putExtra(Intent.EXTRA_TEXT, content)
+                        email.setPackage(info.activityInfo.packageName)
+                        try
+                        {
+                            startActivityForResult(Intent.createChooser(email, ""),100)
 
-            email.putExtra(Intent.EXTRA_EMAIL, address)
-            email.putExtra(Intent.EXTRA_SUBJECT, "$title")
-            email.putExtra(Intent.EXTRA_TEXT, "$content")
-            startActivity(email)
-
+                        }
+                        catch (e:ActivityNotFoundException) {
+                            Toast.makeText(this, "Gmail 앱을 찾는데 실패했습니다.", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            }
         }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == 100){
+            finish()
+        }
+    }
+
+
 
 }
