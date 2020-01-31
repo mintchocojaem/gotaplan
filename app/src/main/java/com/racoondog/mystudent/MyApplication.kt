9632fb06819be0a2e5bd8062dc94e830fa65a72c
void <void0629@gmail.com>
2020-02-01 02:23:07 +0900

    realm 데이터 베이스 관련 주석 추가

--- app/src/main/java/com/racoondog/mystudent/MyApplication.kt
+++ app/src/main/java/com/racoondog/mystudent/MyApplication.kt
@@ -11,9 +11,9 @@ class MyApplication: Application() {
         Realm.init(this)
         var config = RealmConfiguration.Builder()
             .schemaVersion(0)
-            .deleteRealmIfMigrationNeeded() // 개발 중에는 활성화
+            .deleteRealmIfMigrationNeeded() // 개발 중에는 활성화 (데이터 베이스 필드가 변하면 앱 깔릴 때 데이터 베이스가 초기화 됨)
 
-            .migration { realm, oldVersion, newVersion ->
+            .migration { realm, oldVersion, newVersion -> //앱 출시 부터 활성화 (데이터 베이스 정보를 유지하며 업데이트)
                 /*
                 var oldVer = oldVersion // 이전 schema 버전
                 val schema = realm.schema
@@ -21,13 +21,13 @@ class MyApplication: Application() {
                 if (oldVer == 0L)
                 {
                         schema.get("DataModel")
-                        ?.addField("testdata",Int::class.java)
+                        ?.addField("testdata",Int::class.java) // 바뀐 데이터 베이스 필드
                     oldVer++ // 다음 업데이트로 넘어가서 이어서 적용( 사용자가 2,3단 업데이트 가능 ) / 없으면 여러 업데이트 한번에 적용 불가
 
                 }
                 if(oldVer == 1L){
                     schema.get("DataModel")
-                        ?.removeField("testdata")
+                        ?.removeField("testdata") // 바뀐 데이터 베이스 필드
                     oldVer++
                 }
 
