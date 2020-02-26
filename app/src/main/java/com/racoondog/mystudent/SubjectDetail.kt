62531fba122a404aed99919ce1386317ce496ab9
void <void0629@gmail.com>
2020-02-26 20:22:49 +0900

    optimize Test backup

--- app/src/main/java/com/racoondog/mystudent/SubjectDetail.kt
+++ app/src/main/java/com/racoondog/mystudent/SubjectDetail.kt
@@ -1,21 +1,25 @@
 package com.racoondog.mystudent
 
 import android.app.Activity
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.View
 import android.view.View.OnTouchListener
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import io.realm.Realm
 import io.realm.RealmResults
+import io.realm.Sort
 import kotlinx.android.synthetic.main.subject_detail.*
 import kotlinx.android.synthetic.main.time_picker.*
-import kotlinx.android.synthetic.main.time_picker.view.*
 
 
 class SubjectDetail : AppCompatActivity() {
 
     private val realm = Realm.getDefaultInstance()
+    private var subjectColorCode = 0
+    private var dayFlag = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -29,34 +33,11 @@ class SubjectDetail : AppCompatActivity() {
             .findAll()
         val data = subjectData[0]!!
 
-        disableView(subject_detail_view)
+        subjectColorCode = data.subjectColor
+        dayFlag = data.dayFlag
 
+        //disableView(subject_detail_view)
 
-        val startAmPm: String
-        val startHour = subjectData[0]!!.startHour
-        val startMinute = (subjectData[0]!!.startMinute)
-        val textStartHour = when (startHour) {
-            in 13..23 -> startHour - 12
-            else -> startHour
-        }
-        startAmPm = when (startHour) {
-            in 12..23 -> "오후"
-            else -> "오전"
-        }
-
-        val endAmPm: String
-        val endHour = subjectData[0]!!.endHour
-        val endMinute = (subjectData[0]!!.endMinute)
-        val textEndHour = when (endHour) {
-            in 13..23 -> endHour - 12
-            else -> endHour
-        }
-        endAmPm = when (endHour) {
-            in 12..23 -> "오후"
-            else -> "오전"
-        }
-
-        subject_time.text = "$startAmPm $textStartHour:${startMinute}" + " ~ " + "$endAmPm $textEndHour:${endMinute}"
         subject_title.setText(data.title)
         subject_content.setText(data.content)
         themeChange(data.subjectColor)
@@ -92,7 +73,7 @@ class SubjectDetail : AppCompatActivity() {
                 lessonCost_text.isEnabled = true
                 lessonCycle_text.isEnabled = true
 
-                enableView(subject_detail_view)
+                //enableView(subject_detail_view)
 
 
                 lesson_cycle_minus_btn.isEnabled = false
@@ -112,8 +93,10 @@ class SubjectDetail : AppCompatActivity() {
                 lessonCost_text.isEnabled = false
                 lessonCycle_text.isEnabled = false
 
-                disableView(subject_detail_view)
-                subject_detail_view.hidePicker()
+                //disableView(subject_detail_view)
+                //subject_detail_view.hidePicker()
+                //subject_detail_view.subjectDetailSave()
+                themeChange(data.subjectColor)
 
                 lesson_cycle_minus_btn.isEnabled = true
                 lesson_cycle_plus_btn.isEnabled = true
@@ -151,6 +134,66 @@ class SubjectDetail : AppCompatActivity() {
             realm.commitTransaction()
         }
 
+        val scheduleData = realm.where(ScheduleData::class.java).findFirst()!!
+
+        if (scheduleData.scheduleDayFlag == 6){
+            saturday_button.visibility = View.VISIBLE
+        }
+        else if (scheduleData.scheduleDayFlag == 7){
+            saturday_button.visibility = View.VISIBLE
+            sunday_button.visibility = View.VISIBLE
+        }
+
+        when(dayFlag){
+            1 -> monday_button.isChecked = true
+            2 -> tuesday_button.isChecked = true
+            3 -> wednesday_button.isChecked = true
+            4 -> thursday_button.isChecked = true
+            5 -> friday_button.isChecked = true
+            6 -> saturday_button.isChecked = true
+            7 -> sunday_button.isChecked = true
+        }
+
+        monday_button.setOnClickListener {
+            dayFlag = 1
+        }
+        tuesday_button.setOnClickListener {
+            dayFlag = 2
+        }
+        wednesday_button.setOnClickListener {
+            dayFlag = 3
+        }
+        thursday_button.setOnClickListener {
+            dayFlag = 4
+        }
+        friday_button.setOnClickListener {
+            dayFlag = 5
+        }
+        saturday_button.setOnClickListener {
+            dayFlag = 6
+        }
+        sunday_button.setOnClickListener {
+            dayFlag = 7
+        }
+
+        subject_detail_theme_picker_btn_layout.setOnClickListener {
+            val dialog = ColorPickerDialog(this, object :
+                ColorPickerDialog.ICustomDialogEventListener {
+                override fun customDialogEvent(colorCode: Int) {
+                    // Do something with the value here, e.g. set a variable in the calling activity
+                    subjectColorCode = colorCode
+                    subject_detail_theme_picker_btn.backgroundTintList = ColorStateList.valueOf(subjectColorCode)
+
+                }
+            })
+            dialog.show()
+        }
+
+        subject_detail_theme_picker_btn.backgroundTintList = ColorStateList.valueOf(subjectColorCode)
+
+        subject_detail_time_picker.subjectPicker(scheduleData.scheduleStartHour,scheduleData.scheduleEndHour)
+        subject_detail_time_picker.displayTime(data.startHour,data.startMinute.toInt(),data.endHour,data.endMinute.toInt())
+
     }
 
     private fun disableView(v:View){
@@ -181,8 +224,9 @@ class SubjectDetail : AppCompatActivity() {
     }
     override fun onBackPressed() {
 
-        if(time_picker.visibility == View.VISIBLE)subject_detail_view.hidePicker()
-        else super.onBackPressed()
+        //if(time_picker.visibility == View.VISIBLE)//subject_detail_view.hidePicker()
+        //else
+        super.onBackPressed()
     }
 
 
@@ -193,5 +237,114 @@ class SubjectDetail : AppCompatActivity() {
 
     }
 
+    fun subjectDetailSave(){
+
+        val subjectData: RealmResults<SubjectData> = realm.where<SubjectData>(SubjectData::class.java)
+            .equalTo("id",WeekView.ID)
+            .findAll()
+        val data = subjectData[0]!!
+
+        if (end_hour.value == start_hour.value){
+            if(end_minute.displayedValues[end_minute.value].toInt() < start_minute.displayedValues[start_minute.value].toInt()){
+                Toast.makeText(this, "시작 시각이 종료 시각보다 클 수 없습니다.", Toast.LENGTH_SHORT).show()
+            }else if(start_minute.value == end_minute.value){
+                Toast.makeText(this, "시작 시각이 종료 시각과 같을 수 없습니다.", Toast.LENGTH_SHORT).show()
+            } else if((end_minute.displayedValues[end_minute.value].toInt()  - start_minute.displayedValues[start_minute.value].toInt() < 30)) {
+                Toast.makeText(this, "각 과목의 최소 시간은 30분입니다.", Toast.LENGTH_SHORT).show()
+            }else createSubject(dayFlag)
+
+        } else if(end_hour.value - start_hour.value == 1){
+
+            if(end_minute.displayedValues[end_minute.value].toInt()+(60-start_minute.displayedValues[start_minute.value].toInt()) < 30) {
+                Toast.makeText(this, "각 과목의 최소 시간은 30분입니다.", Toast.LENGTH_SHORT).show()
+            } else createSubject(dayFlag)
+
+        }
+        else if (end_hour.value < start_hour.value){
+            Toast.makeText(this, "시작 시각이 종료 시각보다 클 수 없습니다.", Toast.LENGTH_SHORT).show()
+        }
+        else {
+            createSubject(dayFlag)
+        }
+
+        realm.beginTransaction()
+        data.subjectColor = subjectColorCode
+        realm.commitTransaction()
+
+    }
+
+    private fun createSubject(dayFlag: Int){
+
+        val subjectData: RealmResults<SubjectData> = realm.where<SubjectData>(SubjectData::class.java)
+            .equalTo("id",WeekView.ID)
+            .findAll()
+        val data = subjectData[0]!!
+
+        if(checkTime(dayFlag)){
+            Toast.makeText(this,"해당 시간에 다른 과목이 존재합니다.",Toast.LENGTH_SHORT).show()
+        } else {
+            realm.beginTransaction()
+            data.startHour = start_hour.value
+            data.startMinute = start_minute.value.toString()
+            data.endHour = end_hour.value
+            data.endMinute = end_minute.value.toString()
+            data.dayFlag = dayFlag
+            realm.commitTransaction()
+            Toast.makeText(this,"시간이 변경되었습니다.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun checkTime(dayFlag:Int):Boolean{
+
+        var subjectData: RealmResults<SubjectData> =
+            realm.where<SubjectData>(SubjectData::class.java)
+                .equalTo("dayFlag", dayFlag)
+                .notEqualTo("id",WeekView.ID)
+                .findAll()
+        val data = subjectData.sort("startHour", Sort.ASCENDING)
+
+        val pickerTime = arrayListOf<Double>()
+
+        pickerTime.add(start_hour.value.toDouble() + (start_minute.displayedValues[start_minute.value].toDouble() / 100))
+        pickerTime.add(end_hour.value.toDouble() + (end_minute.displayedValues[end_minute.value].toDouble() / 100))
+
+        val checkTime = arrayListOf<Boolean>()
+
+        if(data.size != 0){
+
+            for ( i in data.indices){
+
+                val subjectTime = arrayListOf<Double>()
+
+                subjectTime.add(data[i]!!.startHour.toDouble()+ (data[i]!!.startMinute.toDouble() / 100))
+                subjectTime.add(data[i]!!.endHour.toDouble()+ (data[i]!!.endMinute.toDouble() / 100))
+
+                var checkFlag = when{
+
+                    pickerTime[0] >= subjectTime[1] -> true
+                    pickerTime[0] < subjectTime[0] -> pickerTime[1] <= subjectTime[0]
+                    else -> false
+
+                }
+
+                checkTime.add(checkFlag)
+
+            }
+
+        }else  checkTime.add(true)
+
+        return checkTime.contains(element = false) // checkTime = true -> 시간표 겹침
+
+    }
+
+    fun hidePicker() {
+
+        if(time_picker.visibility == View.VISIBLE) {
+            time_picker.visibility = View.GONE
+            TimePicker(this).changedTextColor(start_picker_layout, true)
+            TimePicker(this).changedTextColor(end_picker_layout, true)
+        }
+
+    }
 
 }
