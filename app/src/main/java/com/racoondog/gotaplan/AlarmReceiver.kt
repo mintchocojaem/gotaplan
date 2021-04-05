fc680a52634618eb7960242ce438447ee3f93adc
void <void0629@gmail.com>
2021-04-05 16:41:46 +0900

    알람 리시버 엘비스 연산자 지움

--- app/src/main/java/com/racoondog/gotaplan/AlarmReceiver.kt
+++ app/src/main/java/com/racoondog/gotaplan/AlarmReceiver.kt
@@ -36,13 +36,13 @@ class AlarmReceiver : BroadcastReceiver() {
         val subjectData: RealmResults<SubjectData> = realm.where<SubjectData>(SubjectData::class.java)
             .equalTo("id",id)
             .findAll()
-        val data = subjectData[0]
+        val data = subjectData[0]!!
 
-        val dayFlag = intent.getIntExtra("dayFlag",data?.dayFlag?:0)
-        val startHour = intent.getIntExtra("startHour",data?.startHour?:0)
-        val startMinute = intent.getStringExtra("startMinute")?:data?.startMinute?:"00"
-        val notification = intent.getIntExtra("notification",data?.notification?:0)
-        val title = intent.getStringExtra("title")?:data?.title?:"None"
+        val dayFlag = intent.getIntExtra("dayFlag",data.dayFlag)
+        val startHour = intent.getIntExtra("startHour",data.startHour)
+        val startMinute = intent.getStringExtra("startMinute")?:data.startMinute
+        val notification = intent.getIntExtra("notification",data.notification)
+        val title = intent.getStringExtra("title")?:data.title
 
         notificationIntent.flags = (Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP)
 
